2aec0b9ec403b589b6aeaf3f9adadc461054b123
Andrea Ieri <andrea.ieri@canonical.com>
2021-05-30 21:15:30 -0400

    Switch to reading and writing xls files directly Add column options

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import pandas as pd
 from spacy.matcher import Matcher
 
 
-def main(dataset, output, model):
+def main(args):
     '''Analyze a csv dataset and add n_active/n_passive columns'''
-    df = pd.read_csv(dataset, index_col='Response#')
-    analyzer = Analyzer(model)
-    df['n_sentences'] = df['Description'].apply(analyzer.count_sentences)
-    df['n_passive'] = df['Description'].apply(analyzer.count_passive)
+    df = pd.read_excel(args.dataset, index_col=args.index_column)
+    analyzer = Analyzer(args.model)
+    df['n_sentences'] = df[args.text_column].apply(analyzer.count_sentences)
+    df['n_passive'] = df[args.text_column].apply(analyzer.count_passive)
     df['n_active'] = df.n_sentences - df.n_passive
     print(df)
-    if output:
-        df.to_csv(output)
+    if args.output:
+        df.to_excel(args.output)
 
 
 class Analyzer():
@@ -46,6 +46,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', required=True)
     parser.add_argument('--output')
-    parser.add_argument('--model', default='en_core_web_sm')
+    parser.add_argument('--model', default='en_core_web_trf')
+    parser.add_argument('--index-column', default='ResponseId')
+    parser.add_argument('--text-column', default='Text')
     args = parser.parse_args()
-    main(args.dataset, args.output, args.model)
+    main(args)
